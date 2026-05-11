a3dabe985566f9aba55a42daa79f9db2082b318f
Moisés Balbino <moisesbalbino239@gmail.com>
2026-05-11 14:42:13 -0300

    fix app routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,8 @@
-import { AppRoutes } from "./routes";
+import {
+  Router,
+} from "./routes";
 
-function App() {
+export default function App() {
 
-  return <AppRoutes />;
+  return <Router />;
 }
-
-export default App;
